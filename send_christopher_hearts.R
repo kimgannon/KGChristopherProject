024d0384885b8aee79dcf81f3c6d99df1ebecaca
heftych1 <74839230+heftych1@users.noreply.github.com>
2020-11-21 17:10:42 -0500

    Added a print reply

--- send_christopher_hearts.R
+++ send_christopher_hearts.R
@@ -2,5 +2,6 @@ send_christopher_hearts <- function(n_hearts) {
   heart <- "<3"
   return_hearts <- rep("<3", n_hearts)
   print("I love you Christopher!")
+  print("I love you too KG")
   return_hearts
 }
